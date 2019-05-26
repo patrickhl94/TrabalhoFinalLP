b3d69c40579f31d675ff3e57749f1e268afe398a
Patrick Lima <patrickhl94@hotmail.com>
2019-05-26 14:55:58 -0300

    Atualização na classe ControleVisitas

--- src/penitenciaria/ControleVisitas.java
+++ src/penitenciaria/ControleVisitas.java
@@ -36,10 +36,10 @@ public class ControleVisitas {
         }
     }
 
-    // REMOVER VISITAS
+    // PESQUISAR VISITAS
     /* Está pesquisa é lenta tendo em vista que irá percorrer sequencialmente o array
     até encontrar o codigo diferente (VERIFICAR COM A PROFESSORA SE EXISTE UMA FORMA MAIS RAPIDA)*/
-    public int removerVisitas(String codigo) {
+    public int pesquisarVisitas(String codigo) {
         int i = 0;
         while (!codigo.equals(this.visitas.get(i).getCod_pessoa()) || i < this.visitas.size()) {
             i++;
@@ -47,4 +47,28 @@ public class ControleVisitas {
         return i;
     }
 
+    // REMOVER VISITAS
+    /*VERIFICAR COM A PROFESSORA COMO REMOVER PELO OBJETO AO INVES DE REMOVER PELO INDICE*/
+    public boolean removerVisitas(String codigo) {
+        int indice = this.pesquisarVisitas(codigo);
+
+        if (indice != this.visitas.size()) {
+            this.visitas.remove(indice);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //ALTERAR DADOS DO VISITANTE
+    public boolean alterarDadosVisit(String codigo, Visitante novo) {
+        int indice = this.pesquisarVisitas(codigo);
+
+        if (indice != this.visitas.size()) {
+            this.visitas.set(indice, novo);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
